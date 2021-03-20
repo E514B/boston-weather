3c06f6c318005c7391f74aed68da2350969db3d0
Eric Brown <ecbrown603@gmail.com>
2021-03-19 22:10:32 -0400

    New boston doc and cleaned naan values This project only needed the boston_weather to have the year and temperature information so I pulled just these two from the SQL database.
    The current code code will take the Boston temperature Naan values
    and replace them with the column mean. I have also attempted to find
    the 7 day average for the temperature column but this needs to be tested
    for accuracy.

--- boston_weather_project.py
+++ boston_weather_project.py
@@ -1,5 +1,15 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 dfboston = pd.read_csv('boston_weather.csv')
 dfglobal = pd.read_csv('global_weather.csv')
 
-print(dfboston)
+
+
+boston_filled = dfboston.fillna(dfboston.mean())
+
+boston_filled['moving_average'] = boston_filled.iloc[:,1].ewm(span=7,adjust=False).mean()
+
+print(boston_filled.head(10))
+
