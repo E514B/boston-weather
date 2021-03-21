e1eb48478c025b22cab2ef69c7b71f496d18a52d
Eric Brown <ecbrown603@gmail.com>
2021-03-21 17:03:03 -0400

    feat: functional code and graph The code now works and graphs. There is additional code intentionally commented out so that it can be kept for comparison if desired. The additional code would graph the two charts taking the first 6 years into consideration with an adjusted rolling(7, min_period=1)

--- boston_weather_project.py
+++ boston_weather_project.py
@@ -11,11 +11,24 @@ boston_filled = dfboston.fillna(dfboston.mean())
 
 boston_filled['MA_7(omit 6)'] = boston_filled.avg_temp.rolling(7).mean()
 
-boston_filled['MA_7'] = boston_filled.avg_temp.rolling(7, min_periods=1).mean()
+#boston_filled['MA_7'] = boston_filled.avg_temp.rolling(7, min_periods=1).mean()
 
 dfglobal['MA_7(omit 6)'] = dfglobal.avg_temp.rolling(7).mean()
 
-dfglobal['MA_7'] = dfglobal.avg_temp.rolling(7, min_periods=1).mean()
+#dfglobal['MA_7'] = dfglobal.avg_temp.rolling(7, min_periods=1).mean()
 
-print(dfglobal.head(10))
-print(boston_filled.head(10))
+print(boston_filled)
+print(dfglobal)
+#Averaged version
+plt.plot(boston_filled.year, boston_filled['MA_7(omit 6)'], label='Boston temp')
+plt.plot(dfglobal.year, dfglobal['MA_7(omit 6)'], label='Global temp')
+
+#Non averaged version
+#plt.plot(boston_filled.year, boston_filled.avg_temp, label='Boston temp')
+#plt.plot(dfglobal.year, dfglobal.avg_temp, label='Global temp')
+
+plt.xlabel("Year")
+plt.ylabel("7 year temperature average")
+plt.title("Boston vs. Global Temperature Comparison")
+plt.legend()
+plt.show()
