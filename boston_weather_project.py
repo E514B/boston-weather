d01c0c76984262871378dee3c6adb9200f41f1f8
Eric Brown <ecbrown603@gmail.com>
2021-03-21 15:27:28 -0400

    feat: global and boston rolling() corrected Both rolling averages for Boston and Global data sets have been corrected to show two results. The first result will omit the first 6 years since there is no data to average. The 2nd result will show results for the first 6 years since min_period= 1

--- boston_weather_project.py
+++ boston_weather_project.py
@@ -9,9 +9,13 @@ dfglobal = pd.read_csv('global_weather.csv')
 
 boston_filled = dfboston.fillna(dfboston.mean())
 
-boston_filled['MA_7'] = boston_filled.avg_temp.rolling(7).mean().shift(-6)
+boston_filled['MA_7(omit 6)'] = boston_filled.avg_temp.rolling(7).mean()
 
-print(boston_filled.tail(20))
+boston_filled['MA_7'] = boston_filled.avg_temp.rolling(7, min_periods=1).mean()
 
+dfglobal['MA_7(omit 6)'] = dfglobal.avg_temp.rolling(7).mean()
 
+dfglobal['MA_7'] = dfglobal.avg_temp.rolling(7, min_periods=1).mean()
 
+print(dfglobal.head(10))
+print(boston_filled.head(10))
